301e2914c4bae87cd3306df80e949dd1bb390e45
chunhaolai <chunhlai.blogger@gmail.com>
2023-12-29 00:03:11 +0800

    Updates

---
+++ 7fab36dd-6b141ad88955a7b6dc27.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchun_hao_lai=self.webpackChunkchun_hao_lai||[]).push([[612],{4125:function(c,l,a){a.d(l,{zku:function(){return n}});const n=(0,a(1625).k)("MailRead24Filled","24",["M10.8 2.34a2.25 2.25 0 0 1 2.4 0l7.74 4.83.22.16L12 12.15 2.83 7.33l.23-.16 7.75-4.83ZM2.06 8.62c-.03.15-.05.3-.05.46v7.67C2 18.55 3.46 20 5.25 20h13.5c1.8 0 3.25-1.46 3.25-3.25V9.08c0-.16-.02-.32-.05-.47l-9.6 5.05a.75.75 0 0 1-.7 0l-9.6-5.04Z"])}}]);
+//# sourceMappingURL=7fab36dd-6b141ad88955a7b6dc27.js.map
